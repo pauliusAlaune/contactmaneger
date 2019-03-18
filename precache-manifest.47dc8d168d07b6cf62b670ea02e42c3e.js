68d9e5c98b23faa40ad6fbddbc12a28fdef05c5e
pauliusAlaune <youremail@email.compaulius.alaune@gmail.com>
2019-03-18 09:15:31 +0200

    Updates

---
+++ precache-manifest.47dc8d168d07b6cf62b670ea02e42c3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72ac90b64cede9e68088",
+    "url": "/contactmaneger/static/js/runtime~main.7c5b6cd7.js"
+  },
+  {
+    "revision": "d8ab7408ed4808221db3",
+    "url": "/contactmaneger/static/js/main.75c97ef3.chunk.js"
+  },
+  {
+    "revision": "19e3b7e3050d7a2e6b61",
+    "url": "/contactmaneger/static/js/2.e93ef14d.chunk.js"
+  },
+  {
+    "revision": "d8ab7408ed4808221db3",
+    "url": "/contactmaneger/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "19e3b7e3050d7a2e6b61",
+    "url": "/contactmaneger/static/css/2.d28eea05.chunk.css"
+  },
+  {
+    "revision": "5c48ac8c463bba90a9ee8edb1e515251",
+    "url": "/contactmaneger/index.html"
+  }
+];
